00d82c38148fafcb29d7b1b82302622ebaeb960a
Dragaar <rosivanyshyn@gmail.com>
2022-08-31 20:04:33 +0300

    111

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -6,7 +6,19 @@ import java.util.regex.Pattern;
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
 
-        // Put your code here
+        if(color == null || color.equals("")) return false;
+
+        Pattern colorPatt = Pattern.compile("^(#){1}" +
+                "(?:[a-fA-F0-9]{3}){1,2}" + //код в межах a-f та чисел 0-9. Може складатись із 3 символів або із 6
+                "$"
+                );
+
+        //^(#)?([a-zA-z0-9]{6})([^\\\\/:*?\"<>|\\r\\n])*$
+        Matcher m = colorPatt.matcher(color);
+
+        if(m.matches()) return true;
+
+        return false;
 
     }
 }
